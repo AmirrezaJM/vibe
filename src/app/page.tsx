373c47e411b71d28c0190547de9d95167b879522
amirrezajm <amirreza.jolani@yahoo.com>
2025-07-08 21:43:19 +0200

    add projects schema to my website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,31 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useTRPC } from '@/trpc/client';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
+import { error } from 'console';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
 export default function Home() {
+  const router = useRouter();
   const [value, setValue] = useState("")
   const trpc = useTRPC();
-  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions())
-  const createdMessage = useMutation(trpc.messages.create.mutationOptions({
-    onSuccess: () => {
-      toast.success("Message created")
+  const createdProject = useMutation(trpc.projects.create.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      router.push(`/projects/${data.id}`)
     }
   }))
 
   return (
-    <div>
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={createdMessage.isPending} onClick={() => createdMessage.mutate({ value: value })}>Invoke Jobs</Button>
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className='max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center'>
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button disabled={createdProject.isPending} onClick={() => createdProject.mutate({ value: value })}>Invoke Jobs</Button>
+      </div>
     </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,10 @@
+import { projectRouter } from '@/modules/projects/server/procedures';
 import { createTRPCRouter } from '../init';
 import { messageRouter } from '@/modules/messages/server/procedures';
 export const appRouter = createTRPCRouter({
-  messages: messageRouter
+  messages: messageRouter,
+  projects: projectRouter
+
   // invoke: baseProcedure.input(z.object({
   //   value: z.string()
   // })).mutation(async({input}) => {
